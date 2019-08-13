74ba424290611762c91a4318bb5a1adc5b420717
Amir Mourali <amourali7135@gmail.com>
2019-08-13 16:46:10 +0800

    WIP

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,46 @@
+class BusinessesController < ApplicationController
+  def index
+    @businesses = Business.all
+  end
+
+  def new
+    @business = Business.new
+  end
+
+  def create
+    @business = Business.new(business_params)
+    user = current_user  #Not sure about this part
+    if @business.save
+      redirect_to @business
+    else
+      render :new
+    end
+  end
+
+  def show
+    @business = Business.find(params[:id])
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    if @business.update(business_params)
+      redirect_to @business
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @business = Business.find(params[:id])
+    @business.destroy
+    redirect_to businesses
+  end
+
+  def edit
+    @business = Business.find(params[:id])
+  end
+
+  def business_params
+    params.require(:business).permit(:location, :name, :description, :logo)
+  end
+end
--- app/models/business_category.rb
+++ app/models/business_category.rb
@@ -1,3 +1,5 @@
 class BusinessCategory < ApplicationRecord
   has_many :business_category_tags
+
+  validates :name, presence: true
 end
