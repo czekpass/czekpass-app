88e7018a5c3581a5aa8f1585e9cc506cc746e2b7
Alexandre Cayla <alex.cayla@gmail.com>
2019-08-13 16:01:55 +0800

    user views tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def business_page
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'b_page', to: 'pages#business_page', as: 'test_business_page'
 end
