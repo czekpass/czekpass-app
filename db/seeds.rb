47dfe6e58cde5836d8eace318aad1fc64efc90e3
meelos <github@myles.uk>
2019-08-14 11:15:10 +0000

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,13 @@ require 'faker'
 # Destroy all the objects before running the seed to prevent duplication
 puts "Destroy everything"
 
+# Reordered these by association because seed failing.
 Perk.destroy_all
 Purchase.destroy_all
 Product.destroy_all
 Employee.destroy_all
-
 Business.destroy_all
 BusinessCategory.destroy_all
-
-
 User.destroy_all
 
 # User faker to create the name then split the name into the first_name and last_name attributes.
@@ -19,38 +17,58 @@ name = Faker::FunnyName.unique.two_word_name
 first_name = name.split(' ')[0]
 last_name = name.split(' ')[1]
 
-# Here we seed the database with a 'Genesis' czekpass product so that we can decrement the providing_product_id to differentiate it from the providing_product_id in the perk seed.
-# To have a product before we run the perk seed, we need to create the associated entities of the product which belongs to a business which belongs to a user.
+# Here we seed the database with a 'Genesis' entity in each table, using standard user so we don't have to signup every time we drop the
 puts "Creating network bootstrap objects (Czeckpass)"
 
-czekpass_user = User.create!(
+  name = Faker::FunnyName.unique.two_word_name
+  first_name = name.split(' ')[0]
+  last_name = name.split(' ')[1]
+
+  user = User.create!(
     email: Faker::Internet.unique.email,
     first_name: first_name,
     last_name: last_name,
      password: '1234567'
-  )
+    )
 
   BusinessCategory.create!(
     name: Faker::Company.industry
     )
 
-czekpass = Business.create!(
-    name: "Czeckpass",
-    description: "Loyalty for nomads",
-    user_id: czekpass_user.id,
+  business = Business.create!(
+    name: Faker::Company.unique.name,
+    description: Faker::Company.bs,
+    user_id: user.id,
     logo: Faker::Company.logo,
     location: Faker::Address.unique.city,
     business_category: BusinessCategory.last
     )
 
+  Employee.create!(
+    business_id: business.id,
+    roles: Faker::Company.profession,
+    user_id: user.id
+    )
 
-czekpass_product = Product.create!(
-    name: "Czekpass sign up" ,
-    description: "Perk from the first purchase",
-    price_cents: 0,
-    category: "Lifestyle" ,
-    business_id: czekpass.id
-  )
+  new_products_array = []
+  rand(1..10).times do
+    product = Product.create!(
+        name: Faker::Appliance.equipment ,
+        description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+        price_cents: Faker::Number.number(digits: 5),
+        category: Faker::Job.field ,
+        business_id: business.id
+       )
+    new_products_array << product
+  end
+
+  Purchase.create!(
+    verified: [true, false].sample,
+    expiration_date: Date.today + rand(1..30),
+    user_id: user.id,
+    product_id: new_products_array.sample.id
+    )
+
 
 # to seed the file 10 times we need a ruby loop. We use the faker gem (required above)
 puts "10 times ruby loop for users, businesses, products and perks"
