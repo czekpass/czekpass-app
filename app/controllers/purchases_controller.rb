836fd4ec4826ea24d7daec6abe1660c42c287488
nick stern <nicholas.stern2@gmail.com>
2019-08-14 12:47:45 +0800

    controller base layout

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,19 @@
 class PurchasesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def purchase_params
+  end
 end
