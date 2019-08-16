db364f88d68223e8c26abcc3b295374ec4db4493
Amir Mourali <amourali7135@gmail.com>
2019-08-16 16:27:32 +0800

    Something

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,9 +7,7 @@ class Product < ApplicationRecord
   # has_many :receiving_products, foreign_key: "receiving_product_id", class_name: "Perk"
   has_many :purchases
 
-  has_many :perks
   # has_many :perks, through: :purchases
-
 
   has_many :perks, foreign_key: "product_id", class_name: "Perk"
 
