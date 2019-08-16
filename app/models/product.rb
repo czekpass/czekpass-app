f5281df241a608353044f118c504401f3b1c968e
Myles Kilgallon <github@myles.uk>
2019-08-16 15:23:57 +0800

    Fixed associations with Savings & Purchases

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,8 @@ class Product < ApplicationRecord
   # has_many :providing_products, foreign_key: "providing_product_id", class_name: "Perk"
   # has_many :receiving_products, foreign_key: "receiving_product_id", class_name: "Perk"
   has_many :purchases
-  #has_many :perks, through: :purchases
-
+  has_many :perks, foreign_key: "product_id", class_name: "Perk"
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :price_cents, presence: true
